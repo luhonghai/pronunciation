b16bc6f822e551abe053ef99cbdc3be461bf2425
hailucmg <hai.lu@c-mg.com>
2014-08-26 17:13:10 +0700

    updae

--- app/src/main/java/com/varma/samples/audiorecorder/RecorderActivity.java
+++ app/src/main/java/com/varma/samples/audiorecorder/RecorderActivity.java
@@ -1,36 +1,31 @@
 package com.varma.samples.audiorecorder;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Activity;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.cmg.android.voicerecorder.R;
+import com.varma.samples.audiorecorder.http.FileCommon;
+import com.varma.samples.audiorecorder.http.UploaderAsync;
+import com.varma.samples.audiorecorder.utils.DeviceUuidFactory;
+import com.varma.samples.audiorecorder.view.MySurfaceView;
 
-public class RecorderActivity extends Activity {
-	private static final int RECORDER_BPP = 16;
-	private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
-	private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
-	private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
-	private static final int RECORDER_SAMPLERATE = 44100;
-	private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
-	private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-	
-	private AudioRecord recorder = null;
-	private int bufferSize = 0;
-	private Thread recordingThread = null;
-	private boolean isRecording = false;
-	
+public class RecorderActivity extends Activity implements RecorderHelper.OnUpdateListener {
+    private RecorderHelper recorderHelper;
+    private MySurfaceView surfaceView;
+	private TextView txtTime;
+    private String mUUID;
+    private Context mContext;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +33,22 @@ public class RecorderActivity extends Activity {
         
         setButtonHandlers();
         enableButtons(false);
-        
-        bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,RECORDER_CHANNELS,RECORDER_AUDIO_ENCODING);
+
+        surfaceView = (MySurfaceView) findViewById(R.id.view_surface_amp);
+        recorderHelper = new RecorderHelper(this);
+        txtTime = (TextView) findViewById(R.id.txtTime);
+        txtTime.setText(recorderHelper.getRecordTime());
+        mUUID = new DeviceUuidFactory(this).getDeviceUuid().toString();
+        ((TextView) findViewById(R.id.txtYourId)).setText("Your ID: " + mUUID);
+        this.mContext = this;
+
+        registerReceiver(mHandleMessageReader, new IntentFilter(UploaderAsync.UPLOAD_COMPLETE_INTENT));
     }
 
 	private void setButtonHandlers() {
 		((Button)findViewById(R.id.btnStart)).setOnClickListener(btnClick);
         ((Button)findViewById(R.id.btnStop)).setOnClickListener(btnClick);
+        ((Button)findViewById(R.id.btnUpload)).setOnClickListener(btnClick);
 	}
 	
 	private void enableButton(int id,boolean isEnable){
@@ -54,199 +58,9 @@ public class RecorderActivity extends Activity {
 	private void enableButtons(boolean isRecording) {
 		enableButton(R.id.btnStart,!isRecording);
 		enableButton(R.id.btnStop,isRecording);
-	}
-	
-	private String getFilename(){
-		String filepath = Environment.getExternalStorageDirectory().getPath();
-		File file = new File(filepath,AUDIO_RECORDER_FOLDER);
-		
-		if(!file.exists()){
-			file.mkdirs();
-		}
-		
-		return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + AUDIO_RECORDER_FILE_EXT_WAV);
-	}
-	
-	private String getTempFilename(){
-		String filepath = Environment.getExternalStorageDirectory().getPath();
-		File file = new File(filepath,AUDIO_RECORDER_FOLDER);
-		
-		if(!file.exists()){
-			file.mkdirs();
-		}
-		
-		File tempFile = new File(filepath,AUDIO_RECORDER_TEMP_FILE);
-		
-		if(tempFile.exists())
-			tempFile.delete();
-		
-		return (file.getAbsolutePath() + "/" + AUDIO_RECORDER_TEMP_FILE);
-	}
-	
-	private void startRecording(){
-		recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-						RECORDER_SAMPLERATE, RECORDER_CHANNELS,RECORDER_AUDIO_ENCODING, bufferSize);
-		
-		recorder.startRecording();
-		
-		isRecording = true;
-		
-		recordingThread = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				writeAudioDataToFile();
-			}
-		},"AudioRecorder Thread");
-		
-		recordingThread.start();
-	}
-	
-	private void writeAudioDataToFile(){
-		byte data[] = new byte[bufferSize];
-		String filename = getTempFilename();
-		FileOutputStream os = null;
-		
-		try {
-			os = new FileOutputStream(filename);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		int read = 0;
-		
-		if(null != os){
-			while(isRecording){
-				read = recorder.read(data, 0, bufferSize);
-				
-				if(AudioRecord.ERROR_INVALID_OPERATION != read){
-					try {
-						os.write(data);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			
-			try {
-				os.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private void stopRecording(){
-		if(null != recorder){
-			isRecording = false;
-			
-			recorder.stop();
-			recorder.release();
-			
-			recorder = null;
-			recordingThread = null;
-		}
-		
-		copyWaveFile(getTempFilename(),getFilename());
-		deleteTempFile();
+        enableButton(R.id.btnUpload,isRecording);
 	}
 
-	private void deleteTempFile() {
-		File file = new File(getTempFilename());
-		
-		file.delete();
-	}
-	
-	private void copyWaveFile(String inFilename,String outFilename){
-		FileInputStream in = null;
-		FileOutputStream out = null;
-		long totalAudioLen = 0;
-		long totalDataLen = totalAudioLen + 36;
-		long longSampleRate = RECORDER_SAMPLERATE;
-		int channels = 2;
-		long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
-		
-		byte[] data = new byte[bufferSize];
-                
-		try {
-			in = new FileInputStream(inFilename);
-			out = new FileOutputStream(outFilename);
-			totalAudioLen = in.getChannel().size();
-			totalDataLen = totalAudioLen + 36;
-			
-			AppLog.logString("File size: " + totalDataLen);
-			
-			WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
-					longSampleRate, channels, byteRate);
-			
-			while(in.read(data) != -1){
-				out.write(data);
-			}
-			
-			in.close();
-			out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void WriteWaveFileHeader(
-			FileOutputStream out, long totalAudioLen,
-			long totalDataLen, long longSampleRate, int channels,
-			long byteRate) throws IOException {
-		
-		byte[] header = new byte[44];
-		
-		header[0] = 'R';  // RIFF/WAVE header
-		header[1] = 'I';
-		header[2] = 'F';
-		header[3] = 'F';
-		header[4] = (byte) (totalDataLen & 0xff);
-		header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-		header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-		header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-		header[8] = 'W';
-		header[9] = 'A';
-		header[10] = 'V';
-		header[11] = 'E';
-		header[12] = 'f';  // 'fmt ' chunk
-		header[13] = 'm';
-		header[14] = 't';
-		header[15] = ' ';
-		header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
-		header[17] = 0;
-		header[18] = 0;
-		header[19] = 0;
-		header[20] = 1;  // format = 1
-		header[21] = 0;
-		header[22] = (byte) channels;
-		header[23] = 0;
-		header[24] = (byte) (longSampleRate & 0xff);
-		header[25] = (byte) ((longSampleRate >> 8) & 0xff);
-		header[26] = (byte) ((longSampleRate >> 16) & 0xff);
-		header[27] = (byte) ((longSampleRate >> 24) & 0xff);
-		header[28] = (byte) (byteRate & 0xff);
-		header[29] = (byte) ((byteRate >> 8) & 0xff);
-		header[30] = (byte) ((byteRate >> 16) & 0xff);
-		header[31] = (byte) ((byteRate >> 24) & 0xff);
-		header[32] = (byte) (2 * 16 / 8);  // block align
-		header[33] = 0;
-		header[34] = RECORDER_BPP;  // bits per sample
-		header[35] = 0;
-		header[36] = 'd';
-		header[37] = 'a';
-		header[38] = 't';
-		header[39] = 'a';
-		header[40] = (byte) (totalAudioLen & 0xff);
-		header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-		header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-		header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-
-		out.write(header, 0, 44);
-	}
 	
 	private View.OnClickListener btnClick = new View.OnClickListener() {
 		@Override
@@ -254,21 +68,80 @@ public class RecorderActivity extends Activity {
 			switch(v.getId()){
 				case R.id.btnStart:{
 					AppLog.logString("Start Recording");
-					
-					enableButtons(true);
-					startRecording();
+
+                    enableButton(R.id.btnStart,false);
+                    enableButton(R.id.btnStop,true);
+                    enableButton(R.id.btnUpload, false);
+                    recorderHelper.startRecording();
 							
 					break;
 				}
 				case R.id.btnStop:{
-					AppLog.logString("Start Recording");
-					
-					enableButtons(false);
-					stopRecording();
+					AppLog.logString("Stop Recording");
+                    enableButton(R.id.btnStart,true);
+                    enableButton(R.id.btnStop,false);
+                    enableButton(R.id.btnUpload, true);
+                    recorderHelper.stopRecording();
 					
 					break;
 				}
+                case R.id.btnUpload:{
+                    AppLog.logString("Start Uploading");
+                    ((Button) findViewById(R.id.btnUpload)).setText("Uploading...");
+                    enableButton(R.id.btnStart, false);
+                    enableButton(R.id.btnStop,false);
+                    enableButton(R.id.btnUpload,false);
+
+                    UploaderAsync async  = new UploaderAsync(mContext, getResources().getString(R.string.upload_url));
+                    Map<String, String> params = new HashMap<String,String>();
+                    String fileName = recorderHelper.getFilename();
+                    File tmp = new File(fileName);
+                    if (tmp.exists()) {
+                        params.put(FileCommon.PARA_FILE_NAME, tmp.getName());
+                        params.put(FileCommon.PARA_FILE_PATH,tmp.getAbsolutePath());
+                        params.put(FileCommon.PARA_FILE_TYPE, "audio/wav");
+                        params.put("uuid", mUUID);
+                        async.execute(params);
+                    }
+                    break;
+                }
 			}
 		}
-	}; 
+	};
+
+    @Override
+    public void update(final short[] bytes, final int length, final float sampleLength) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                surfaceView.setData(bytes, length, sampleLength);
+                txtTime.setText(recorderHelper.getRecordTime());
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    private final BroadcastReceiver mHandleMessageReader = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getExtras().containsKey(UploaderAsync.UPLOAD_COMPLETE_INTENT)) {
+                enableButton(R.id.btnStart, true);
+                enableButton(R.id.btnStop,false);
+                ((Button) findViewById(R.id.btnUpload)).setText("Upload");
+                enableButton(R.id.btnUpload,false);
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            unregisterReceiver(mHandleMessageReader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ app/src/main/java/com/varma/samples/audiorecorder/http/UploaderAsync.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2013. CMG Ltd All rights reserved.
+ *
+ * This software is the confidential and proprietary information of CMG
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with CMG.
+ */
+package com.varma.samples.audiorecorder.http;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+
+import com.varma.samples.audiorecorder.http.exception.UploaderException;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+/**
+ * DOCME
+ *
+ * @author $Author$
+ * @version $Revision$
+ * @Creator Hai Lu
+ * @Last changed: $LastChangedDate$
+ */
+public class UploaderAsync extends AsyncTask<Map<String, String>, Void, String> {
+    public static final String UPLOAD_COMPLETE_INTENT = "com.cmg.android.voicerecorder.UploaderAysnc";
+    private final Context context;
+    private final String uploadUrl;
+    public UploaderAsync(Context context, String uploadUrl) {
+        this.context = context;
+        this.uploadUrl = uploadUrl;
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(Map<String, String>... params) {
+        try {
+            StringBuffer results = new StringBuffer();
+            if (params != null && params.length > 0) {
+                for (Map<String, String> param : params) {
+                    results.append(FileUploader.upload(param, uploadUrl));
+                }
+            }
+            return results.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UploaderException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String v) {
+        Intent intent = new Intent(UPLOAD_COMPLETE_INTENT);
+        intent.putExtra(UPLOAD_COMPLETE_INTENT, "true");
+        context.sendBroadcast(intent);
+        Toast.makeText(context, "Uploading completed!", Toast.LENGTH_LONG).show();
+        super.onPostExecute(v);
+    }
+
+}
