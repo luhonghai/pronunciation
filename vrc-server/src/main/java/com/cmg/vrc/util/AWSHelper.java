3285267ca666a406cb80220b568d893430f891b5
hailucmg <hai.lu@c-mg.com>
2015-06-05 12:10:15 +0700

    Update

--- vrc-server/src/main/java/com/cmg/vrc/util/AWSHelper.java
+++ vrc-server/src/main/java/com/cmg/vrc/util/AWSHelper.java
@@ -3,7 +3,10 @@ package com.cmg.vrc.util;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.policy.actions.ElasticBeanstalkActions;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClient;
+import com.amazonaws.services.elasticbeanstalk.model.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
@@ -16,6 +19,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,11 +35,15 @@ public class AWSHelper {
 
     private AmazonS3 s3client;
 
+    private AWSElasticBeanstalkClient beanstalkClient;
+
     private String bucketName;
 
     public AWSHelper() {
         if (ENABLE_AWS) {
-            s3client = new AmazonS3Client(new BasicAWSCredentials("AKIAIW47GXTESF26RHZQ", "o+o1yyloDI4yRfwx0ffTAmk5nEc7iU7Bi32ur/gy"));
+            BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAIW47GXTESF26RHZQ", "o+o1yyloDI4yRfwx0ffTAmk5nEc7iU7Bi32ur/gy");
+            s3client = new AmazonS3Client(credentials);
+            beanstalkClient = new AWSElasticBeanstalkClient(credentials);
             bucketName = Configuration.getValue(Configuration.AWS_S3_BUCKET_NAME);
         }
     }
@@ -128,15 +136,36 @@ public class AWSHelper {
         }
     }
 
+
+    public void getEnvironmentInfo(String envId) {
+        RetrieveEnvironmentInfoResult result =  beanstalkClient.retrieveEnvironmentInfo(new RetrieveEnvironmentInfoRequest().withEnvironmentId(envId));
+        List<EnvironmentInfoDescription> infos = result.getEnvironmentInfo();
+        for (EnvironmentInfoDescription info : infos) {
+            System.out.println("Instance ID: " + info.getEc2InstanceId()
+                    //+ ". Info type: " + info.getInfoType()
+                    + ". Message: " + info.getMessage());
+        }
+    }
+
+    public void restartBeanstalkApp(String envId) {
+        beanstalkClient.restartAppServer(new RestartAppServerRequest().withEnvironmentId(envId));
+
+    }
+
+    public void rebuildEnvironment(String envId) {
+        beanstalkClient.rebuildEnvironment(new RebuildEnvironmentRequest().withEnvironmentId(envId));
+    }
+
     public static void main(String[] args) {
         AWSHelper awsHelper = new AWSHelper();
-        System.out.println("Start uploading");
+        awsHelper.getEnvironmentInfo("accenteasytomcat-PRD-1");
+       // System.out.println("Start uploading");
         //awsHelper.upload("sphinx-data/wsj-en-us.zip", new File("/Volumes/DATA/CMG/git/pronunciation/sphinx-data/wsj-en-us.zip"));
-        awsHelper.upload("sphinx-data/dict/brit-a-z.txt", new File("/Volumes/DATA/CMG/git/pronunciation/sphinx-data/words/british/brit-a-z.txt"));
+       // awsHelper.upload("sphinx-data/dict/brit-a-z.txt", new File("/Volumes/DATA/CMG/git/pronunciation/sphinx-data/words/british/brit-a-z.txt"));
         //awsHelper.upload("sphinx-data/dict/beep-1.0", new File("/Volumes/DATA/OSX/luhonghai/Desktop/beep/beep-1.0"));
         //awsHelper.upload("sphinx-data/dict/cmuphonemedict", new File("/Volumes/DATA/CMG/git/pronunciation/sphinx-data/dict/cmuphonemedict"));
 
-        System.out.println("Complete uploading");
+        //System.out.println("Complete uploading");
 //        System.out.println("Start downloading & unzip");
         //awsHelper.downloadAndUnzip("sphinx-data/wsj-en-us.zip", new File("/Users/cmg/git/pronunciation/sphinx-data/tmp"));
         //awsHelper.download("", new File(FileUtils.getTempDirectory(), UUIDGenerator.generateUUID()));
