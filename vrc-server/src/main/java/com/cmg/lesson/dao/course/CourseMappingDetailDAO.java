92abfad9af72c941a39b642fb1fe6100dbbad1d1
Hoang Nguyen <hoang.nguyen@c-mg.com>
2015-10-23 09:31:11 +0700

    add function for Management Objective

--- vrc-server/src/main/java/com/cmg/lesson/dao/course/CourseMappingDetailDAO.java
+++ vrc-server/src/main/java/com/cmg/lesson/dao/course/CourseMappingDetailDAO.java
@@ -3,7 +3,11 @@ package com.cmg.lesson.dao.course;
 import com.cmg.lesson.data.jdo.course.Course;
 import com.cmg.lesson.data.jdo.course.CourseMappingDetail;
 import com.cmg.vrc.data.dao.DataAccess;
+import com.cmg.vrc.util.PersistenceManagerHelper;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.jdo.metadata.TypeMetadata;
 import java.util.List;
 
 /**
@@ -11,6 +15,95 @@ import java.util.List;
  */
 public class CourseMappingDetailDAO extends DataAccess<CourseMappingDetail> {
     public CourseMappingDetailDAO(){super(CourseMappingDetail.class);}
+
+    /**
+     *  use for get latest version in table
+     * @return latest version
+     * @throws Exception
+     */
+    public int getLatestVersion() throws Exception{
+        int version = 0;
+        PersistenceManager pm = PersistenceManagerHelper.get();
+        Query q = pm.newQuery("SELECT max(version) FROM " + CourseMappingDetail.class.getCanonicalName());
+        try {
+            if (q != null) {
+                version = (int) q.execute();
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (q!= null)
+                q.closeAll();
+            pm.close();
+        }
+        return version;
+    }
+
+    /**
+     *
+     * @param idCourse
+     * @param idObjectiveOrTest
+     * * @param idLevel
+     * @return true if question exist
+     * @throws Exception
+     */
+    public boolean checkExist(String idCourse, String idObjectiveOrTest, String idLevel) throws Exception{
+        List<CourseMappingDetail> list = list("WHERE idCourse==:1 && idChild== :2 && idLevel==:3 && isDeleted==:4", idCourse,idObjectiveOrTest,idLevel,false);
+        if(list!=null && list.size() > 0){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param idLevel
+     * @return
+     */
+    public boolean updateDeleted(String idLevel){
+        boolean check = false;
+        PersistenceManager pm = PersistenceManagerHelper.get();
+        TypeMetadata metaRecorderSentence = PersistenceManagerHelper.getDefaultPersistenceManagerFactory().getMetadata(CourseMappingDetail.class.getCanonicalName());
+        Query q = pm.newQuery("javax.jdo.query.SQL", "UPDATE " + metaRecorderSentence.getTable() + " SET isDeleted= ? WHERE idLevel=?");
+        try {
+            q.execute(true,idLevel);
+            check=true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (q!= null)
+                q.closeAll();
+            pm.close();
+        }
+
+        return check;
+    }
+
+    /**
+     *
+     * @param
+     * @return
+     */
+    public boolean updateDeleted(String idCourse,String idObjective, String idLevel){
+        boolean check = false;
+        PersistenceManager pm = PersistenceManagerHelper.get();
+        TypeMetadata metaRecorderSentence = PersistenceManagerHelper.getDefaultPersistenceManagerFactory().getMetadata(CourseMappingDetail.class.getCanonicalName());
+        Query q = pm.newQuery("javax.jdo.query.SQL", "UPDATE " + metaRecorderSentence.getTable() + " SET isDeleted = true WHERE idCourse=? and idChild=? and idLevel=?");
+        try {
+            q.execute(idCourse,idObjective, idLevel);
+            check=true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (q!= null)
+                q.closeAll();
+            pm.close();
+        }
+
+        return check;
+    }
 
     /**
      *
---
+++ vrc-server/src/main/java/com/cmg/lesson/services/course/CourseMappingDetailService.java
@@ -0,0 +1,135 @@
+package com.cmg.lesson.services.course;
+
+
+import com.cmg.lesson.dao.course.CourseMappingDetailDAO;
+import com.cmg.lesson.data.dto.course.CourseDTO;
+import com.cmg.lesson.data.dto.level.LevelDTO;
+import com.cmg.lesson.data.jdo.course.CourseMappingDetail;
+import com.cmg.lesson.data.jdo.level.Level;
+import com.cmg.lesson.services.level.LevelService;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lantb on 2015-10-21.
+ */
+public class CourseMappingDetailService {
+    private static final Logger logger = Logger.getLogger(CourseMappingDetailService.class
+            .getName());
+    private String SUCCESS = "success";
+    private String ERROR = "error";
+
+
+    /**
+     *  use for get max version
+     * @return max version in table
+     */
+    public int getMaxVersion(){
+        int version = 0;
+        CourseMappingDetailDAO dao = new CourseMappingDetailDAO();
+        try {
+            version = dao.getLatestVersion();
+        }catch (Exception e){
+            logger.info("can not get max version in table because : " + e.getMessage());
+        }
+        return version +1;
+    }
+
+    /**
+     *
+     * @param idCourse
+     * @param idLevel
+     * @return
+     */
+    public CourseDTO addMappingDetail(String idCourse, String idObjective, String idLevel, boolean isTest){
+        CourseDTO dto = new CourseDTO();
+        CourseMappingDetailDAO dao = new CourseMappingDetailDAO();
+        String message;
+        try {
+            boolean condition = checkExist(idCourse, idObjective, idLevel);
+            if(!condition){
+                CourseMappingDetail obj = new CourseMappingDetail();
+                obj.setIdCourse(idCourse);
+                obj.setIdLevel(idLevel);
+                obj.setIdChild(idObjective);
+                obj.setVersion(getMaxVersion());
+                obj.setIsTest(isTest);
+                obj.setIsDeleted(false);
+                dao.create(obj);
+                message = SUCCESS;
+            }else{
+                message = ERROR +": can not add MappingDetail!";
+            }
+        }catch (Exception e){
+            message = ERROR +": An error has been occurred in server!" + e.getMessage();
+            logger.error("can not add mapping because : " + e.getMessage());
+        }
+        dto.setMessage(message);
+        return dto;
+    }
+
+    /**
+     *
+     * @param idCourse
+     * @param idLevel
+     * @return
+     */
+    public CourseDTO removeDetail(String idCourse, String idObjective, String idLevel){
+        CourseDTO dto = new CourseDTO();
+        CourseMappingDetailDAO dao = new CourseMappingDetailDAO();
+        String message;
+        try {
+            if(dao.updateDeleted(idCourse, idObjective, idLevel)){
+                message = SUCCESS;
+            }else{
+                message = ERROR +": An error has been occurred in server!";
+            }
+        }catch (Exception e){
+            message = ERROR +": An error has been occurred in server!";
+            logger.error("can not add mapping because : " + e);
+        }
+        dto.setMessage(message);
+        return dto;
+    }
+
+
+    /**
+     *
+     * @param idLevel
+     * @return
+     */
+    public String removeDetail( String idLevel){
+        CourseMappingDetailDAO dao = new CourseMappingDetailDAO();
+        String message;
+        try {
+            if(dao.updateDeleted(idLevel)){
+                message = SUCCESS;
+            }else{
+                message = ERROR +": An error has been occurred in server!";
+            }
+        }catch (Exception e){
+            message = ERROR +": An error has been occurred in server!" + e.getMessage();
+            logger.error("can not add mapping because : " + e.getMessage());
+        }
+        return message;
+    }
+
+    /**
+     *
+     * @param idCourse
+     * @param idLevel
+     * @return
+     */
+    public boolean checkExist(String idCourse, String idObjective, String idLevel){
+        CourseMappingDetailDAO dao = new CourseMappingDetailDAO();
+        boolean check = false;
+        try {
+            check = dao.checkExist(idCourse, idObjective, idLevel);
+        }catch (Exception e){
+            logger.error("can not check exited: "  + e.getMessage());
+        }
+        return check;
+    }
+}
