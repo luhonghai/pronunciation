a4aab49a31874a2de41d8914a951c18cb9ae7aec
Nam Bui <nam.bui@c-mg.com>
2015-11-16 14:38:32 +0700

    fix

--- vrc-server/src/main/java/com/cmg/vrc/servlet/DatabaseDataHandler.java
+++ vrc-server/src/main/java/com/cmg/vrc/servlet/DatabaseDataHandler.java
@@ -1,7 +1,9 @@
 package com.cmg.vrc.servlet;
 
 import com.cmg.vrc.common.Constant;
+import com.cmg.vrc.data.dao.impl.DatabaseVersionDAO;
 import com.cmg.vrc.data.dao.impl.DictionaryVersionDAO;
+import com.cmg.vrc.data.jdo.DatabaseVersion;
 import com.cmg.vrc.data.jdo.DictionaryVersion;
 import com.cmg.vrc.util.AWSHelper;
 import com.google.gson.Gson;
@@ -30,7 +32,7 @@ public class DatabaseDataHandler extends BaseServlet {
 
         public Double recordsFiltered;
 
-        List<DictionaryVersion> data;
+        List<DatabaseVersion> data;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -45,14 +47,14 @@ public class DatabaseDataHandler extends BaseServlet {
         }
         try {
             if (!StringUtils.isEmpty(action)) {
-                final DictionaryVersionDAO dao = new DictionaryVersionDAO();
+                final DatabaseVersionDAO dao = new DatabaseVersionDAO();
                 final AWSHelper awsHelper = new AWSHelper();
                 if (action.equalsIgnoreCase("load")) {
 
                 } else if (action.equalsIgnoreCase("link_generate")) {
                     String id = request.getParameter("id");
                     if (!StringUtils.isEmpty(id)) {
-                        DictionaryVersion model = dao.getById(id);
+                        DatabaseVersion model = dao.getById(id);
                         if (model != null) {
                             out.write(awsHelper.generatePresignedUrl(Constant.FOLDER_DATABASE
                                     + "/"
@@ -66,7 +68,7 @@ public class DatabaseDataHandler extends BaseServlet {
                 } else if (action.equalsIgnoreCase("select")) {
                     String id = request.getParameter("id");
                     if (!StringUtils.isEmpty(id)) {
-                        DictionaryVersion model = dao.getById(id);
+                        DatabaseVersion model = dao.getById(id);
                         if (model != null) {
                             dao.removeSelected();
                             model.setSelectedDate(new Date(System.currentTimeMillis()));
