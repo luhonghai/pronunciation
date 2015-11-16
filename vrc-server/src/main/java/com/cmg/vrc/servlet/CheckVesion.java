9ffb5c69891d4af91babe7448b4a71a7a097e6ed
Nam Bui <nam.bui@c-mg.com>
2015-11-16 11:27:13 +0700

    fix

--- vrc-server/src/main/java/com/cmg/vrc/servlet/CheckVesion.java
+++ vrc-server/src/main/java/com/cmg/vrc/servlet/CheckVesion.java
@@ -20,11 +20,14 @@ public class CheckVesion extends HttpServlet {
     private static String PARA_PROFILE = "profile";
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        
+        String message="success";
+
+        out.print(message);
+
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+     doPost(request,response);
     }
 }
