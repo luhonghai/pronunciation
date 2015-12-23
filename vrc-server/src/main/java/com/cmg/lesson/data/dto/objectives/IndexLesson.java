b1c2727cf30f5706bfe7a3e633b29eadc099bbaa
Nam Bui <nam.bui@c-mg.com>
2015-12-23 14:33:09 +0700

    fix

---
+++ vrc-server/src/main/java/com/cmg/lesson/data/dto/objectives/IndexLesson.java
@@ -0,0 +1,29 @@
+package com.cmg.lesson.data.dto.objectives;
+
+/**
+ * Created by CMG Dev156 on 10/14/2015.
+ */
+public class IndexLesson {
+
+
+
+    private String idObjects;
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getIdObjects() {
+        return idObjects;
+    }
+
+    public void setIdObjects(String idObjects) {
+        this.idObjects = idObjects;
+    }
+
+}
---
+++ vrc-server/src/main/java/com/cmg/lesson/data/dto/objectives/IndexOBJ.java
@@ -0,0 +1,32 @@
+package com.cmg.lesson.data.dto.objectives;
+
+import com.cmg.lesson.data.jdo.objectives.Objective;
+
+import java.util.List;
+
+/**
+ * Created by CMG Dev156 on 10/14/2015.
+ */
+public class IndexOBJ {
+
+    private String message;
+    private List<Objective> data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Objective> getData() {
+        return data;
+    }
+
+    public void setData(List<Objective> data) {
+        this.data = data;
+    }
+
+
+}
