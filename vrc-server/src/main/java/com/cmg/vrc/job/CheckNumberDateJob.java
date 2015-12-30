4bb042196c45dae20dc29285e5503c3ee2e6aa1f
Nam Bui <nam.bui@c-mg.com>
2015-12-30 11:52:39 +0700

    fix

--- vrc-server/src/main/java/com/cmg/vrc/job/CheckNumberDateJob.java
+++ vrc-server/src/main/java/com/cmg/vrc/job/CheckNumberDateJob.java
@@ -51,32 +51,46 @@ public class CheckNumberDateJob {
         @Override
         public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
             logger.info("Start Check job");
+            AppDetailDAO appDetailDAO=new AppDetailDAO();
+
             UserDAO userDAO=new UserDAO();
             NumberDateDAO numberDateDAO=new NumberDateDAO();
+            List<User> users=null;
             MailService mailService=new MailService();
             List<String> recipients = new ArrayList<String>();
-            String subject=null;
-            String message = null;
             try {
-                List<User> users=userDAO.users();
+                List<AppDetail> appDetails=appDetailDAO.listAll();
+                users=userDAO.users();
                 Date date=new Date(System.currentTimeMillis());
                 int number=numberDateDAO.numberDate().getNumberDate();
                 java.util.Calendar cal1 = java.util.Calendar.getInstance();
                 cal1.setTime(date);
+                cal1.set(Calendar.MILLISECOND, 0);
+                cal1.set(Calendar.SECOND,0);
+                cal1.set(Calendar.MINUTE, 0);
+                cal1.set(Calendar.HOUR, 0);
                 java.util.Calendar cal2 = java.util.Calendar.getInstance();
-
                 if(users!=null){
-                    for(Object user:users){
-                        Object[] array = (Object[]) user;
-                        cal2.setTime((Date)array[2]);
+                    for(User user:users){
+                        cal2.setTime(user.getCreatedDate());
+                        cal2.set(Calendar.MILLISECOND, 0);
+                        cal2.set(Calendar.SECOND,0);
+                        cal2.set(Calendar.MINUTE,0);
+                        cal2.set(Calendar.HOUR, 0);
                         cal2.add(java.util.Calendar.DATE, number);
                         if(cal2.equals(cal1)){
-                            recipients.add(array[1].toString());
+                            logger.info("send mail to expired user :"+user.getUsername() + " created date: " + user.getCreatedDate());
+                            recipients.add(user.getUsername());
                         }
                     }
                 }
-                String[] recipients1 = recipients.toArray(new String[recipients.size()]);
-                mailService.sendEmail(recipients1,subject,message);
+                if (recipients.size() > 0) {
+                    logger.info("Send expired email notification to " + recipients.size() + " users");
+                    String[] recipients1 = recipients.toArray(new String[recipients.size()]);
+                    mailService.sendEmail(recipients1, appDetails.get(0).getSubject(), appDetails.get(0).getMessage());
+                } else {
+                    logger.info("No expired user found. Skip email sender.");
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -84,50 +98,50 @@ public class CheckNumberDateJob {
         }
     }
     public static void main(String[] args){
-        AppDetailDAO appDetailDAO=new AppDetailDAO();
-
-        UserDAO userDAO=new UserDAO();
-        NumberDateDAO numberDateDAO=new NumberDateDAO();
-        List<User> users=null;
-        MailService mailService=new MailService();
-        List<String> recipients = new ArrayList<String>();
-        try {
-            List<AppDetail> appDetails=appDetailDAO.listAll();
-            users=userDAO.users();
-            Date date=new Date(System.currentTimeMillis());
-            int number=numberDateDAO.numberDate().getNumberDate();
-            java.util.Calendar cal1 = java.util.Calendar.getInstance();
-            cal1.setTime(date);
-            cal1.set(Calendar.MILLISECOND, 0);
-            cal1.set(Calendar.SECOND,0);
-            cal1.set(Calendar.MINUTE,0);
-            cal1.set(Calendar.HOUR, 0);
-            java.util.Calendar cal2 = java.util.Calendar.getInstance();
-            if(users!=null){
-                for(User user:users){
-                    cal2.setTime(user.getCreatedDate());
-                    cal2.set(Calendar.MILLISECOND, 0);
-                    cal2.set(Calendar.SECOND,0);
-                    cal2.set(Calendar.MINUTE,0);
-                    cal2.set(Calendar.HOUR, 0);
-                    cal2.add(java.util.Calendar.DATE, number);
-                    if(cal2.equals(cal1)){
-                        logger.info("send mail to expired user :"+user.getUsername() + " created date: " + user.getCreatedDate());
-                        recipients.add(user.getUsername());
-                    }
-                }
-            }
-            if (recipients.size() > 0) {
-                logger.info("Send expired email notification to " + recipients.size() + " users");
-                String[] recipients1 = recipients.toArray(new String[recipients.size()]);
-                mailService.sendEmail(recipients1, appDetails.get(0).getSubject(), appDetails.get(0).getMessage());
-            } else {
-                logger.info("No expired user found. Skip email sender.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        logger.info("Complete StopEnvironmentJob job");
+//        AppDetailDAO appDetailDAO=new AppDetailDAO();
+//
+//        UserDAO userDAO=new UserDAO();
+//        NumberDateDAO numberDateDAO=new NumberDateDAO();
+//        List<User> users=null;
+//        MailService mailService=new MailService();
+//        List<String> recipients = new ArrayList<String>();
+//        try {
+//            List<AppDetail> appDetails=appDetailDAO.listAll();
+//            users=userDAO.users();
+//            Date date=new Date(System.currentTimeMillis());
+//            int number=numberDateDAO.numberDate().getNumberDate();
+//            java.util.Calendar cal1 = java.util.Calendar.getInstance();
+//            cal1.setTime(date);
+//            cal1.set(Calendar.MILLISECOND, 0);
+//            cal1.set(Calendar.SECOND,0);
+//            cal1.set(Calendar.MINUTE,0);
+//            cal1.set(Calendar.HOUR, 0);
+//            java.util.Calendar cal2 = java.util.Calendar.getInstance();
+//            if(users!=null){
+//                for(User user:users){
+//                    cal2.setTime(user.getCreatedDate());
+//                    cal2.set(Calendar.MILLISECOND, 0);
+//                    cal2.set(Calendar.SECOND,0);
+//                    cal2.set(Calendar.MINUTE,0);
+//                    cal2.set(Calendar.HOUR, 0);
+//                    cal2.add(java.util.Calendar.DATE, number);
+//                    if(cal2.equals(cal1)){
+//                        logger.info("send mail to expired user :"+user.getUsername() + " created date: " + user.getCreatedDate());
+//                        recipients.add(user.getUsername());
+//                    }
+//                }
+//            }
+//            if (recipients.size() > 0) {
+//                logger.info("Send expired email notification to " + recipients.size() + " users");
+//                String[] recipients1 = recipients.toArray(new String[recipients.size()]);
+//                mailService.sendEmail(recipients1, appDetails.get(0).getSubject(), appDetails.get(0).getMessage());
+//            } else {
+//                logger.info("No expired user found. Skip email sender.");
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        logger.info("Complete StopEnvironmentJob job");
 
     }
 
