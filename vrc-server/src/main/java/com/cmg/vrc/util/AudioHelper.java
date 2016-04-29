256933ba9e9749db6ac26838a9ce4141cab74a1b
hailucmg <hai.lu@c-mg.com>
2016-04-29 11:22:46 +0700

    Update

--- vrc-server/src/main/java/com/cmg/vrc/util/AudioHelper.java
+++ vrc-server/src/main/java/com/cmg/vrc/util/AudioHelper.java
@@ -1,6 +1,11 @@
 package com.cmg.vrc.util;
 
 import com.cmg.vrc.processor.CommandExecutor;
+import com.cmg.vrc.processor.CustomFFMPEGLocator;
+import com.cmg.vrc.properties.Configuration;
+import it.sauronsoftware.jave.AudioAttributes;
+import it.sauronsoftware.jave.Encoder;
+import it.sauronsoftware.jave.EncodingAttributes;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -13,6 +18,32 @@ public class AudioHelper {
 
     public static boolean convertToWav(File target, File output) {
         return convertToWav(target, output, null);
+    }
+
+    public static boolean convertToWavEncoder(File target, File output) {
+        AudioAttributes audio = new AudioAttributes();
+        //  audio.setBitRate(128000);
+        audio.setChannels(1);
+        audio.setSamplingRate(16000);
+        EncodingAttributes attrs = new EncodingAttributes();
+        attrs.setFormat("wav");
+        attrs.setAudioAttributes(audio);
+        String env = Configuration.getValue(Configuration.SYSTEM_ENVIRONMENT);
+        Encoder encoder;
+        if (env.equalsIgnoreCase("prod") || env.equalsIgnoreCase("sat")
+                || env.equalsIgnoreCase("int")
+                || env.equalsIgnoreCase("aws")) {
+            encoder = new Encoder(new CustomFFMPEGLocator());
+        } else {
+            encoder = new Encoder(new CustomFFMPEGLocator.MacFFMPEGLocator());
+        }
+        try {
+            encoder.encode(target, output, attrs);
+        } catch (Exception e) {
+            // ingore
+            e.printStackTrace();
+        }
+        return output.exists();
     }
 
     public static boolean convertToWav(File target, File output, CommandExecutor.CommandListener listener) {
